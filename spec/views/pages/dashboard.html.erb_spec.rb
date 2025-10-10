ac80d8e5ae0c10e90e0163be27f39b1587f8ffdc
Dio Arayan <drarayan1994@gmail.com>
2025-10-11 07:52:23 +0800

    install pry

--- spec/views/pages/dashboard.html.erb_spec.rb
+++ spec/views/pages/dashboard.html.erb_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "pages/dashboard.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    assign(:documents, [
+      Document.create!(
+        title: "First Title",
+        description: "MyText"
+      ),
+      Document.create!(
+        title: "Second Title",
+        description: "MyText"
+      )
+    ])
+  end
+
+  it "renders a list of documents" do
+    render
+    cell_selector = 'div>p'
+    assert_select cell_selector, text: Regexp.new("Title".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new("MyText".to_s), count: 2
+  end
 end
