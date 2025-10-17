c91af240a0633752ba48626d1cf08e3614636ea4
Dio Arayan <drarayan1994@gmail.com>
2025-10-18 00:50:40 +0800

    add columns to user table and integrate form and fullname method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_11_142630) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_12_085425) do
   create_table "documents", charset: "utf8mb3", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -31,6 +31,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_11_142630) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
